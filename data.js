a7b2d9546e91348c3c150616f3deb9c81bc3feab
boram0219lee <boram0219.lee@gmail.com>
2026-06-09 13:12:39 +0900

    Add files via upload

--- data.js
+++ data.js
@@ -1,55 +1,10 @@
 const works = [
   {
-    name: "홍길동",
-    title: "새벽 숲의 빛",
-    description: "5월 아침 숲길의 상쾌한 공기 속에 깃든 존재",
-    link: "https://editor.p5js.org/example/sketches/xxxxx",
-    thumbnail: "thumbnails/홍길동_새벽숲의빛.jpg",
-    input: "mouse",
-    sound: false
-  },
-  {
-    name: "김민지",
-    title: "비 오는 오후",
-    description: "빗방울 사이를 유영하는 작은 생명체",
-    link: "https://editor.p5js.org/example/sketches/xxxxx",
-    thumbnail: "thumbnails/김민지_비오는오후.jpg",
-    input: "mouse",
-    sound: true
-  },
-  {
-    name: "이준서",
-    title: "새벽 4시",
-    description: "고요한 어둠 속에서 천천히 숨쉬는 존재",
-    link: "https://editor.p5js.org/example/sketches/xxxxx",
-    thumbnail: "thumbnails/이준서_새벽4시.jpg",
-    input: "webcam",
-    sound: false
-  },
-  {
-    name: "박서연",
-    title: "리듬 속으로",
-    description: "좋아하는 음악의 비트에 반응하는 생명체",
-    link: "https://editor.p5js.org/example/sketches/xxxxx",
-    thumbnail: "thumbnails/박서연_리듬속으로.jpg",
-    input: "mouse",
-    sound: true
-  },
-  {
-    name: "최유진",
-    title: "고양이의 시선",
-    description: "느린 움직임엔 무심하고 빠른 움직임엔 반응하는 존재",
-    link: "https://editor.p5js.org/example/sketches/xxxxx",
-    thumbnail: "thumbnails/최유진_고양이의시선.jpg",
-    input: "arduino",
-    sound: false
-  },
-  {
-    name: "정다은",
-    title: "카페의 온기",
-    description: "따뜻한 연기 속에 모습을 드러내는 생명체",
-    link: "https://editor.p5js.org/example/sketches/xxxxx",
-    thumbnail: "thumbnails/정다은_카페의온기.jpg",
+    name: "강다은",
+    title: "얼음 결정",
+    description: "몽환적인 분위기 속에 깃든 얼음 결정 생명체",
+    link: "https://editor.p5js.org/daeunkang/sketches/vVQd4a_gI",
+    thumbnail: "강다은_얼음결정.jpg",
     input: "mouse",
     sound: true
   }
